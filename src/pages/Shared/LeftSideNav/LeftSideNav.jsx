2964bff9fc0418c974806d144ab60ed1bcefdeac
Md. Naimul Hasan Emon <hasan15-5601@diu.edu.bd>
2025-01-19 22:47:55 +0600

    Right side navbar added

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import './LeftSideNav.css'
+
 const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
   return (
     <div>
-      <p className='text-2xl'>This is left side nav</p>
+      <p className='text-2xl poppins-medium'>All Category</p>
+      <div className="ml-7">
+        {categories.map((category) => (
+          <div>
+            <Link
+            to='/'
+            className='flex p-3 poppins-medium text-gray-500 hover:text-black hover:bg-base-200 rounded-lg' key={category.id}>
+              {category.name}
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,71 @@
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Swimming from "../../../assets/Images/swimming.png";
+import Class from "../../../assets/Images/class.png";
+import PlayGround from "../../../assets/Images/playground.png";
+
 const RightSideNav = () => {
-    return (
-        <div>
-            <p className="text-2xl">This is right side nav</p>
+  return (
+    <div>
+      {/* Login With */}
+      <div>
+        <p className='text-3xl mb-7'>Login With</p>
+        <div className='my-2'>
+          <button className='btn w-full flex items-center gap-1 border-2 border-black hover:bg-white hover:text-blue-400 hover:border-blue-400'>
+            <FaGoogle className='text-xl' />
+            Login With Google
+          </button>
         </div>
-    );
+        <div>
+          <div>
+            <button className='btn w-full flex items-center gap-1 border-2 border-black hover:bg-white hover:text-blue-400 hover:border-blue-400'>
+              <FaGithub className='text-xl' />
+              Login With Github
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Find Us On */}
+      <div className='mt-10'>
+        <p className='text-3xl mb-7'>Find Us On</p>
+        <div className='border-[1px] border-slate-400 rounded-lg'>
+          <Link className='p-3 flex gap-2 text-lg items-center border rounded-t-lg'>
+            <FaFacebook className='text-xl' />
+            <span>Facebook</span>
+          </Link>
+          <Link className='p-3 flex gap-2 text-lg items-center border-x'>
+            <FaTwitter className='text-xl' />
+            <span>Twitter</span>
+          </Link>
+          <Link className='p-3 flex gap-2 text-lg items-center border rounded-b-lg'>
+            <FaInstagram className='text-xl' />
+            <span>Instagram</span>
+          </Link>
+        </div>
+      </div>
+
+      {/* Q-Zone */}
+      <div className='bg-base-200 px-3 my-10'>
+          <p className='text-3xl mb-5 pt-3'>Q-Zone</p>
+         <div>
+          <img src={Swimming} alt='Swimming' />
+        </div>
+        <div>
+          <img src={Class} alt='Class' />
+        </div>
+        <div>
+          <img src={PlayGround} alt='PlayGround' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RightSideNav;
